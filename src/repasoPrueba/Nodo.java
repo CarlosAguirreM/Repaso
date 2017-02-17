4bc3a710a30dd703267820d0b136bba273bbc517
CarlosAguirreM <darksorlac1197@gmail.com>
2017-02-17 01:26:47 -0500

    Update Nodo.java

--- src/repasoPrueba/Nodo.java
+++ src/repasoPrueba/Nodo.java
@@ -2,53 +2,6 @@ package repasoPrueba;
 
 public class Nodo {
 	
-/*	private int contenido;
-	private Nodo siguiente;
-	private Nodo anterior;
-	
-	public Nodo(){
-		
-		this.contenido=0;
-		this.siguiente=null;
-		this.anterior=null;
-		
-	}
-	
-	public Nodo(int cont, Nodo sig, Nodo ant){
-		
-		this.contenido=cont;
-		this.siguiente=sig;
-		this.anterior=ant;
-		
-	}
-	
-	public Nodo(int cont, Nodo sig){
-	
-		this.contenido=cont;
-		this.siguiente=sig;
-		this.anterior=null;
-		
-	}	
-	
-	public int getContenido() {
-		return contenido;
-	}
-	public void setContenido(int contenido) {
-		this.contenido = contenido;
-	}
-	public Nodo getSiguiente() {
-		return siguiente;
-	}
-	public void setSiguiente(Nodo siguiente) {
-		this.siguiente = siguiente;
-	}
-	public Nodo getAnterior() {
-		return anterior;
-	}
-	public void setAnterior(Nodo anterior) {
-		this.anterior = anterior;
-	}*/
-	
 	private String contenido;
 	private Nodo siguiente;
 	private Nodo anterior;
